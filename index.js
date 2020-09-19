5cfd4d5612309cb9493b02bc60b53bf133c2ad2a
= <=>
2020-09-19 18:59:06 +0700

    method post

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 const pug = require('pug')
+
 var app = express()
 var port = 3000
 
@@ -11,6 +13,11 @@ var users = [
 
 app.set('view engine', 'pug')
 app.set('views', './views')
+
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 
 app.get('/', function (req, res) {
    res.render('home', {
@@ -37,13 +44,23 @@ app.get('/users/search', (req, res)=>{
        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
     });
 
-    console.log(matchUsers);
-
     //render list user match
     res.render('users/index', {
         users: matchUsers
     });
 });
+
+app.get('/users/create',(req, res)=>{
+   res.render('users/create');
+});
+
+app.post('/users/create',(req, res)=>{
+    users.push({name: req.body.todo});
+    console.log(req.body.todo);
+    res.redirect('/users');
+});
+
+
 
 app.listen(port, function () {
     console.log('Server listening');
